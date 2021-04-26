c4d19129cc7c5dec00510ee5bf4718305e3bbacc
edmko <edmko195@gmail.com>
2021-04-26 17:01:48 +0300

    added resources

---
+++ app/src/main/java/ua/edmko/unocounter/UnoApplication.kt
@@ -0,0 +1,8 @@
+package ua.edmko.unocounter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UnoApplication: Application() {
+}
---
+++ app/src/main/java/ua/edmko/unocounter/ui/theme/Dimensions.kt
@@ -0,0 +1,5 @@
+package ua.edmko.unocounter.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val baseDimension = 18.dp
